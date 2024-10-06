313b74348e5f171d577faf0cd28a53a3cc0d721d
Nassim Ezzaamari <mail@mail.to>
2024-10-07 00:50:05 +0200

    Maybe maybe TP2

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,16 @@
+package fr.univavignon.pokedex.api;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IPokedexFactoryTest {
+
+
+    @Test
+    void testTTTTTTTT() {
+        assertEquals(5, 5);
+    }
+
+}
