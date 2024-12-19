63298f00a918866aa357f7dab60e9dec7b3bfd80
nassim <nassim.ezzaamari@alumni.univ-avignon.fr>
2024-12-19 09:34:35 +0100

    Coup de pouce à codecov 🤮

--- src/test/java/fr/univavignon/pokedex/api/RocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/RocketPokemonFactoryTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class IRocketPokemonFactoryTest {
+public class RocketPokemonFactoryTest {
 
     private IPokemonFactory pokemonFactory;
     private Pokemon validMetadata;
