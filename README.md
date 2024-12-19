fff4301bccab9d1a24c650b9907af2bcfdcc1a94
nassim <nassim.ezzaamari@alumni.univ-avignon.fr>
2024-12-19 09:04:58 +0100

    TP 6 + De tests 🤮

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # UCE Génie Logiciel Avancé : Techniques de tests
 
 [![CircleCI](https://circleci.com/gh/nassimezzaamari/ceri-m1-techniques-de-test/tree/master.svg?style=svg)](https://circleci.com/gh/nassimezzaamari/ceri-m1-techniques-de-test/tree/master) [![codecov](https://codecov.io/github/nassimezzaamari/ceri-m1-techniques-de-test/graph/badge.svg?token=2YTKGVI636)](https://codecov.io/github/nassimezzaamari/ceri-m1-techniques-de-test)
-
 ![Checkstyle](target/badges/checkstyle-result.svg)
+
+## Documentation 
+https://nassimezzaamari.github.io/ceri-m1-techniques-de-test/fr/univavignon/pokedex/api/package-summary.html
+
+
 
 Author : Nassim Ezzaamari
 Group : M1 ILSEN Classique 
--- src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
@@ -4,7 +4,7 @@ package fr.univavignon.pokedex.api;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class IRocketPokemonFactoryTest {
 
@@ -39,5 +39,71 @@ public class IRocketPokemonFactoryTest {
     public void testValidCandy() throws PokedexException {
         Pokemon actualMetadata = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
         assertEquals(validMetadata.getCandy(), actualMetadata.getCandy());
+    }
+
+
+    @Test
+    public void testCreateValidPokemon() {
+        Pokemon pokemon = pokemonFactory.createPokemon(1, 500, 50, 2000, 3);
+
+        assertNotNull(pokemon);
+        assertEquals("Bulbasaur", pokemon.getName());
+        assertEquals(1, pokemon.getIndex());
+        assertEquals(500, pokemon.getCp());
+        assertEquals(50, pokemon.getHp());
+        assertEquals(2000, pokemon.getDust());
+        assertEquals(3, pokemon.getCandy());
+        assertEquals(1.0, pokemon.getIv(), 0.001);
+    }
+
+    @Test
+    public void testCreateAshPikachu() {
+        Pokemon pokemon = pokemonFactory.createPokemon(-1, 500, 50, 2000, 3);
+
+        assertNotNull(pokemon);
+        assertEquals("Ash's Pikachu", pokemon.getName());
+        assertEquals(-1, pokemon.getIndex());
+        assertEquals(1000, pokemon.getAttack());
+        assertEquals(1000, pokemon.getDefense());
+        assertEquals(1000, pokemon.getStamina());
+        assertEquals(0.0, pokemon.getIv(), 0.001);
+    }
+
+    @Test
+    public void testCreateMissingno() {
+        Pokemon pokemon = pokemonFactory.createPokemon(999, 500, 50, 2000, 3);
+
+        assertNotNull(pokemon);
+        assertEquals("MISSINGNO", pokemon.getName());
+        assertEquals(999, pokemon.getIndex());
+        assertTrue(pokemon.getAttack() >= 0 && pokemon.getAttack() <= 100);
+        assertTrue(pokemon.getDefense() >= 0 && pokemon.getDefense() <= 100);
+        assertTrue(pokemon.getStamina() >= 0 && pokemon.getStamina() <= 100);
+    }
+
+    @Test
+    public void testRandomStatGeneration() {
+        Pokemon pokemon = pokemonFactory.createPokemon(1, 500, 50, 2000, 3);
+
+        assertTrue(
+                pokemon.getAttack() >= 0 && pokemon.getAttack() <= 100);
+        assertTrue(
+                pokemon.getDefense() >= 0 && pokemon.getDefense() <= 100);
+        assertTrue(
+                pokemon.getStamina() >= 0 && pokemon.getStamina() <= 100);
+    }
+
+    @Test
+    public void testMultipleRandomPokemon() {
+        // Test de la RNG
+        Pokemon pokemon1 = pokemonFactory.createPokemon(1, 500, 50, 2000, 3);
+        Pokemon pokemon2 = pokemonFactory.createPokemon(1, 500, 50, 2000, 3);
+
+        boolean allStatsIdentical =
+                pokemon1.getAttack() == pokemon2.getAttack() &&
+                        pokemon1.getDefense() == pokemon2.getDefense() &&
+                        pokemon1.getStamina() == pokemon2.getStamina();
+
+        assertFalse(allStatsIdentical);
     }
 }
